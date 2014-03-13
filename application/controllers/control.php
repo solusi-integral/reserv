bab6da71fcf435439474dd4cc55fba86b5a6c186
indra <indra@INTEGRAL-IND.integral.local>
2014-03-13 15:51:05 +0700

    Changed document title

--- application/controllers/control.php
+++ application/controllers/control.php
@@ -29,6 +29,10 @@ class Control extends CI_Controller {
             $data['pesan']      = $this->status();
             $data['today_sum']  = $this->today();
             $data['today_red']  = $this->today_red();
+            $data['today_perf'] = $this->today_performance();
+            $data['today_gree'] = $this->today_green();
+            $data['yeste_gree'] = $this->yesterday_green();
+            //$data['twod_gree']  = $this->twodays_green();
             $this->load->view('index', $data);
 	}
         
@@ -45,13 +49,13 @@ class Control extends CI_Controller {
             $this->load->helper('date');
             $this->load->model('report_model','',TRUE);
             $now                = time();
-            $UL                 = 0;
+            $UL                 = 1;
             $DL                 = 15;  
             $this->db->where('Results <', $UL);
             $this->db->or_where('Results >', $DL);
             $this->db->from('result');
             $red                = $this->db->count_all_results();
-            $data               = $red/2;
+            $data               = $red;
             return $data;
         }
         
@@ -131,15 +135,51 @@ class Control extends CI_Controller {
             $this->load->helper('date');
             $time = now();
             $today  = date("Y-m-d", $time);
-            $UL                 = 0;
+            $UL                 = 1;
             $DL                 = 15;  
             $this->db->where('Date =', $today);
             $this->db->where('Results <', $UL);
             $this->db->or_where('Results >', $DL);
             $this->db->from('result');
             $today_total                = $this->db->count_all_results();
-            $red    = $today_total/2;
+            $red    = $today_total;
             return $red;
+        }
+        
+        function today_green()
+        {
+            $total  = $this->today();
+            $red    = $this->today_red();
+            $green  = $total-$red;
+            $perce1     = round(($green/$total*100),2);
+            return $perce1;
+        }
+        
+        function today_performance()
+        {
+            $status = $this->today_green();
+            if ($status > 90){
+                $pesan  = 'Great';
+            }
+            else if ($status > 80){
+                $pesan  = 'Good';
+            }
+            else if ($status > 75){
+                $pesan  = 'Fair';
+            }
+            else {
+                $pesan  = 'Bad';
+            }
+            return $pesan; 
+        }
+        
+        function today_last1()
+        {
+            $this->load->model('report_model','',TRUE);
+            $this->db->select('Results');
+            $query  = $this->db->get('result', 5);
+            $row['ro1']   = $query->row(1);
+            return $row;
         }
         
         function yesterday()
@@ -147,9 +187,73 @@ class Control extends CI_Controller {
             $this->load->helper('date');
             $time = now();
             $today  = date("Y-m-d", $time - 86400);
-            return $today;
+            $this->db->where('Date =', $today);
+            $this->db->from('result');
+            $today_total                = $this->db->count_all_results();
+            return $today_total;
         }
-      
+        
+        function yesterday_red()
+        {
+            $this->load->helper('date');
+            $time = now();
+            $today  = date("Y-m-d", $time - 86400);
+            $UL                 = 1;
+            $DL                 = 15;  
+            $this->db->where('Date =', $today);
+            $this->db->where('Results <', $UL);
+            $this->db->or_where('Results >', $DL);
+            $this->db->from('result');
+            $today_total                = $this->db->count_all_results();
+            $red    = $today_total;
+            return $red;
+        }
+        
+        function yesterday_green()
+        {
+            $total  = $this->yesterday();
+            $red    = $this->yesterday_red();
+            $green  = $total-$red;
+            $perce1     = round(($green/$total*100),2);
+            return $perce1;
+        }
+        /*
+        function twodays()
+        {
+            $this->load->helper('date');
+            $time = now();
+            $today  = date("Y-m-d", $time - (60*60*24*2));
+            $this->db->where('Date =', $today);
+            $this->db->from('result');
+            $today_total                = $this->db->count_all_results();
+            return $today_total;
+        }
+        
+        function twodays_red()
+        {
+            $this->load->helper('date');
+            $time = now();
+            $today  = date("Y-m-d", $time - (60*60*24*2));
+            $UL                 = 1;
+            $DL                 = 15;  
+            $this->db->where('Date =', $today);
+            $this->db->where('Results <', $UL);
+            $this->db->or_where('Results >', $DL);
+            $this->db->from('result');
+            $today_total                = $this->db->count_all_results();
+            $red    = $today_total;
+            return $red;
+        }
+        
+        function twodays_green()
+        {
+            $total  = $this->twodays();
+            $red    = $this->twodays_red();
+            $green  = $total-$red;
+            $perce1     = round(($green/$total*100),2);
+            return $perce1;
+        }
+      */
 }
 
 /* End of file welcome.php */
