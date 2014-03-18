afd813510b4fb8abc3ce815c2ea8d6b1960618d0
indra <indra@INTEGRAL-IND.integral.local>
2014-03-18 15:50:26 +0700

    Insert Link to each last 6 races

--- application/views/index.php
+++ application/views/index.php
@@ -196,7 +196,7 @@
 							<ul class="flat medium">
                                                             <?php
                                                             foreach ($last6 as $row){
-								echo '<li><a href="data/edit/'. $row->ID .'"><span class="spark_bar small random_number_5 spark_inline"></span>'. $row->Name .' clicked '.$row->Location .' with '. $row->Results .' as the result.</a></li>';
+								echo '<li><a href="data/edit/'. $row->ID .'" target="_blank"><span class="spark_bar small random_number_5 spark_inline"></span>'. $row->Name .' clicked '.$row->Location .' with '. $row->Results .' as the result.</a></li>';
                                                             }
                                                             ?>
 							</ul>
