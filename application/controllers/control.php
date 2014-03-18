a71e3bc9180f000eaf607944f773efb7174a8ce9
indra <indra@INTEGRAL-IND.integral.local>
2014-03-18 11:51:18 +0700

    Added Documentation

--- application/controllers/control.php
+++ application/controllers/control.php
@@ -3,22 +3,29 @@
 class Control extends CI_Controller {
 
 	/**
-	 * Index Page for this controller.
+	 * Index Page for Remote Reporting System Dashboard 
 	 *
 	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
+	 * 		http://example.com/index.php/control
 	 *
 	 * So any other public methods not prefixed with an underscore will
 	 * map to /index.php/welcome/<method_name>
 	 * @see http://codeigniter.com/user_guide/general/urls.html
+         * 
+         * @package     Reserv
+         * @author      CV Solusi Integral
+         * @copyright   Copyright (c) 2014 CV Solusi Integral
+         * @link http://www.solusi-integral.co.id CV Solusi Integral
 	 */
 	public function index()
 	{
+            /**
+             * Index
+             *
+             * This is a route based class which allow other function to interact with view and model.
+             * 
+             * @data    mixed
+             */
             $this->load->model('report_model','',TRUE);
             $data['sumrace']    = $this->sumrace();
             $data['red']        = $this->redrace();
