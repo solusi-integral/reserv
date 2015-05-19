95d95e6287e2d4492a8eb335a63753b313fb9686
indra <indra@INTEGRAL-IND.integral.local>
2015-05-19 09:34:41 +0700

    Switch to HTTPS connection

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://reserv.solusi-integral.co.id/';
+$config['base_url']	= 'https://reserv.solusi-integral.co.id/';
 
 /*
 |--------------------------------------------------------------------------
