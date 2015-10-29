140096805b4e77653864772e55e9b9c8c739ac74
indra <indra@INTEGRAL-IND.integral.local>
2015-10-29 13:09:13 +0700

    Fixing Loading Overlay

--- application/views/includes/core/document_foot.php
+++ application/views/includes/core/document_foot.php
@@ -1,9 +1,11 @@
-		<?//php include '../../includes/dialogs/dialog_register.php'?>
+		<!-- 
+                <?//php include '../../includes/dialogs/dialog_register.php'?>
 		<?//php include '../dialogs/dialog_form.php'?>
 		<?//php include '../../includes/dialogs/dialog_delete.php'?>
 		<?//php include '../../includes/dialogs/dialog_welcome.php'?>
 		<?//php include '../../includes/dialogs/dialog_logout.php'?>
-		
+                -->
+  
 		<!-- Removed to avoid loading overlay at all times. This bug has been requested by at -->
                 <!-- Cancelled due to reduced functionality -->
 		<div id="loading_overlay">
@@ -11,9 +13,10 @@
 				<img src="<?php echo $this->config->base_url(); ?>images/interface/loading.gif" alt="loading" />
 			</div>
 		</div>
-
+                <!--
 		<?//php include '../../includes/components/template_options.php'?>
-
+                -->    
+                
 		</div>
 	</body>
 </html>
