1916a749f908d6c88b62a50c5f892abed5343c6b
indra <indra@INTEGRAL-IND.integral.local>
2015-10-29 12:08:47 +0700

    Restoring Loading Overlay

--- application/views/includes/core/document_foot.php
+++ application/views/includes/core/document_foot.php
@@ -4,13 +4,14 @@
 		<?//php include '../../includes/dialogs/dialog_welcome.php'?>
 		<?//php include '../../includes/dialogs/dialog_logout.php'?>
 		
-		<!-- Removed to avoid loading overlay at all times. This bug has been requested by at 
+		<!-- Removed to avoid loading overlay at all times. This bug has been requested by at -->
+                <!-- Cancelled due to reduced functionality -->
 		<div id="loading_overlay">
 			<div class="loading_message round_bottom">
 				<img src="<?//php echo $this->config->base_url(); ?>images/interface/loading.gif" alt="loading" />
 			</div>
 		</div>
-		-->
+
 		<?//php include '../../includes/components/template_options.php'?>
 
 		</div>
