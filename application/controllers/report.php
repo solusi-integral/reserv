ac83d885df5fa9a80976f670eb6466caf0d054df
indra <indra@INTEGRAL-IND.integral.local>
2015-11-30 08:24:32 +0700

    HTTP Header and JSON response for every remote API calls #12
    Change from view to private function to display JSON output

--- application/controllers/report.php
+++ application/controllers/report.php
@@ -120,15 +120,15 @@ class Report extends CI_Controller {
         
         private function __jsonoutput($data)
         {
-            $this->output->set_output(json_encode($data));
             $this->output->set_content_type('application/json');
+            $this->output->set_status_header('201');
+            $this->output->set_output(json_encode($data));
             //$this->output->set_header("HTTP/1.0 200 OK");
             //$this->output->set_header("HTTP/1.1 200 OK");
             //$this->output->set_header('Last-Modified: '.gmdate('D, d M Y H:i:s', $last_update).' GMT');
             //$this->output->set_header("Cache-Control: no-store, no-cache, must-revalidate");
             //$this->output->set_header("Cache-Control: post-check=0, pre-check=0");
             //$this->output->set_header("Pragma: no-cache");
-            //$this->output->set_status_header('201');
         }
         
         private function __mailonmissed5()
