9cada1ab5105f23a67b0d02bfe64ee2bb0a4bc12
indra <indra@INTEGRAL-IND.integral.local>
2015-12-01 14:51:47 +0700

    Integrate Reserv with Freshdesk #15
    Deploying to server.

--- application/controllers/report.php
+++ application/controllers/report.php
@@ -178,6 +178,63 @@ class Report extends CI_Controller {
             //$this->output->set_header("HTTP/1.1 200 OK");
         }
         
+        public function freshdsk_create()
+        {
+            $fd_domain = "https://cvsolusiintegral.freshdesk.com";
+            $token = "ggXySu214rbWhkDJpAKU";
+            $password = "X";
+            $data = array(
+                "helpdesk_ticket" => array(
+                    "description" => "Some details on the issue ...",
+                    "subject" => "Support needed..",
+                    "email" => "tom@outerspace.com",
+                    "priority" => 1,
+                    "status" => 2
+                ),
+                "cc_emails" => "ram@freshdesk.com,diana@freshdesk.com"
+            );
+            $json_body = json_encode($data, JSON_FORCE_OBJECT | JSON_PRETTY_PRINT);
+            $header[] = "Content-type: application/json";
+            $connection = curl_init("$fd_domain/helpdesk/tickets.json");
+            curl_setopt($connection, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($connection, CURLOPT_HTTPHEADER, $header);
+            curl_setopt($connection, CURLOPT_HEADER, false);
+            curl_setopt($connection, CURLOPT_USERPWD, "$token:$password");
+            curl_setopt($connection, CURLOPT_POST, true);
+            curl_setopt($connection, CURLOPT_POSTFIELDS, $json_body);
+            curl_setopt($connection, CURLOPT_VERBOSE, 1);
+            $response = curl_exec($connection);
+            echo $response;
+        }
+        
+        private function __freshdeskupdate()
+        {
+            $fd_domain = "https://cvsolusiintegral.freshdesk.com";
+            $token = "YOUR_FRESHDESK_API_TOKEN";
+            $password = "X";
+            $custom_field = array(
+                      "weapon_1" => "Laser Gun" );
+            $data = array(
+                "helpdesk_ticket" => array(
+                    "priority" => 4,
+                    "status" => 3,
+                    "custom_field" => $custom_field
+                )
+            );
+            $json_body = json_encode($data, JSON_FORCE_OBJECT | JSON_PRETTY_PRINT);
+            $header[] = "Content-type: application/json";
+            $connection = curl_init("$fd_domain/helpdesk/tickets/[ticket_id].json");
+            curl_setopt($connection, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($connection, CURLOPT_HTTPHEADER, $header);
+            curl_setopt($connection, CURLOPT_HEADER, false);
+            curl_setopt($connection, CURLOPT_USERPWD, "$token:$password");
+            curl_setopt($connection, CURLOPT_POSTFIELDS, $json_body);
+            curl_setopt($connection, CURLOPT_CUSTOMREQUEST, 'PUT');
+            curl_setopt($connection, CURLOPT_VERBOSE, 1);
+            $response = curl_exec($connection);
+            echo $response;
+        }
+        
         private function __osticket($name,$email,$subject,$message)
         {
             /**
