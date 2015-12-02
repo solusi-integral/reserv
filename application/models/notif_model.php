9ab497bd7227c17ad23ee96ee8c7bc7695457902
Indra Kurniawan <indramgl@users.noreply.github.com>
2015-12-02 17:44:05 +0700

    Task #15 - Integrate Reserv with Freshdesk
    Working on new model for notification

--- application/models/notif_model.php
+++ application/models/notif_model.php
@@ -16,16 +16,24 @@ class Notif_model extends CI_Model{
     
     public function insert($ticket_id)
     {
+        // Create an array to enter the data
         $data = array(
-            'ticket_id' => $ticket_id
+            'ticket_id' => $ticket_id,
+            'notif_5'   => 0,
+            'notif_10'  => 0,
+            'notif_30'  => 0
         );
+        // Return the data to function caller
         return $this->db->insert('notification',$data);
     }
     
     public function lookup($time)
     {
+        // Lookup database for spesific criteria
         $this->db->where('date =', $time);
+        // Get data from notification table
         $this->db->from('notification');
+        // Execute the query and store it
         $query  = $this->db->get();
         // return the query
         return $query->result();
@@ -33,15 +41,21 @@ class Notif_model extends CI_Model{
     
     public function count($time)
     {
+        // Filter table for spesific data
         $this->db->where('date =', $time);
+        // Get the data from notification table
         $this->db->from('notification');
+        // Count the number of row 
         $query  = $this->db->count_all_results();
+        // Return the query to the caller
         return $query;
     }
     
     public function findticket($id)
     {
+        // Find spesific data from the database
         $this->db->where('ticket_id =', $id);
+        // Set the table and store the data to variable
         $query   = $this->db->count_all_results('notification');
         return $query;
     }
