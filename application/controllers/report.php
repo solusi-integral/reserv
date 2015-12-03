1d2efe8e3ab5c5e1eff2d75cd9e779529d376d16
Indra Kurniawan <indramgl@users.noreply.github.com>
2015-12-03 09:52:31 +0700

    Task #15 - Integrate Reserv with Freshdesk
    Working on new model for notification

--- application/controllers/report.php
+++ application/controllers/report.php
@@ -221,6 +221,7 @@ class Report extends CI_Controller {
                 {
                     $ticket_id  = $row->display_id;
                 }
+                
                 $this->notif_model->insert($ticket_id);
                 echo $ticket_id;
             }
