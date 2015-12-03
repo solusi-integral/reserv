94c799c7edba2817937d10d3f2f13d583e760794
Indra Kurniawan <indramgl@users.noreply.github.com>
2015-12-03 10:14:33 +0700

    Task #15 - Integrate Reserv with Freshdesk
    Working on new model for notification

--- application/controllers/report.php
+++ application/controllers/report.php
@@ -264,6 +264,7 @@ class Report extends CI_Controller {
               // php5.4 & below: 'helpdesk_note[attachments][][resource]' =>  "@x.png"
               //'helpdesk_note[attachments][][resource]' =>  curl_file_create("data/x.png", "image/png", "x.png")
             );
+            $json_body  = json_encode($payload, JSON_FORCE_OBJECT | JSON_PRETTY_PRINT);
             $header[] = "Content-type: application/json";
             $url = "$FD_ENDPOINT/helpdesk/tickets/".$id."/conversations/note.json";
             $ch = curl_init($url);
