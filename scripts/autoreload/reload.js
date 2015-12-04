f1aa11423921ee52238330d68f7aa60d9c89d7cf
Indra Kurniawan <indramgl@users.noreply.github.com>
2015-12-04 10:19:05 +0700

    Change cache-control from 60s expired to 86.400 (1 day)

--- scripts/autoreload/reload.js
+++ scripts/autoreload/reload.js
@@ -8,13 +8,13 @@
 $(document).ready(function() {
 $.ajaxSetup({ cache: false }); // This part addresses an IE bug. without it, IE will only load the first number and will never refresh
 setInterval(function() {
-$('#pjax_total').load('https://reserv.solusi-integral.co.id/index.php/pajax/pjax_total');
-$('#pjax_green').load('https://reserv.solusi-integral.co.id/index.php/pajax/pjax_green');
-$('#pjax_status').load('https://reserv.solusi-integral.co.id/index.php/pajax/pjax_status');
-$('#pjax_person').load('https://reserv.solusi-integral.co.id/index.php/pajax/pjax_person');
-$('#pjax_last6').load('https://reserv.solusi-integral.co.id/index.php/pajax/pjax_last6');
-$('#pjax_today').load('https://reserv.solusi-integral.co.id/index.php/pajax/pjax_today');
-$('#pjax_today2').load('https://reserv.solusi-integral.co.id/index.php/pajax/pjax_today');
+$('#pjax_total').load('https://reserv.solusi-integral.co.id/pajax/pjax_total');
+$('#pjax_green').load('https://reserv.solusi-integral.co.id/pajax/pjax_green');
+$('#pjax_status').load('https://reserv.solusi-integral.co.id/pajax/pjax_status');
+$('#pjax_person').load('https://reserv.solusi-integral.co.id/pajax/pjax_person');
+$('#pjax_last6').load('https://reserv.solusi-integral.co.id/pajax/pjax_last6');
+$('#pjax_today').load('https://reserv.solusi-integral.co.id/pajax/pjax_today');
+$('#pjax_today2').load('https://reserv.solusi-integral.co.id/pajax/pjax_today');
 }, 1234); // the "3000" here refers to the time to refresh the div. it is in milliseconds.
 });
 // ]]>
