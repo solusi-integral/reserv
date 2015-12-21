ac1071e8d1f85ec9a56d470596f5229db494348a
Indra Kurniawan <indramgl@users.noreply.github.com>
2015-12-21 10:32:55 +0700

    Task #16 - Weekly Email Report
    Added new helper class for a frequently used function across various controller.

--- application/controllers/control.php
+++ application/controllers/control.php
@@ -28,6 +28,7 @@ class Control extends CI_Controller {
 	public function index()
 	{
             $this->load->model('report_model','',TRUE);
+            $this->load->helper('performance');
             $data['sumrace']    = $this->sumrace();
             $data['red']        = $this->redrace();
             $data['perce']      = $this->green_percentage();
