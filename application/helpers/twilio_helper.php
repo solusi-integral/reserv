93d768a9d2dfa20bed6a6ca042514b643f4a10f4
Indra Kurniawan <indramgl@users.noreply.github.com>
2015-12-28 19:04:08 +0700

    This commit also include Twillio Helper.

---
+++ application/helpers/twilio_helper.php
@@ -0,0 +1,35 @@
+<?php
+
+if (!class_exists('Services_Twilio')) {
+	/**
+	 * The main Twilio.php file contains an autoload method for its dependent
+	 * classes, we only need to include the one file manually.
+	 */
+	include_once(APPPATH.'libraries/Services/Twilio.php');
+}
+
+/**
+ * Return a twilio services object.
+ *
+ * Since we don't want to create multiple connection objects we
+ * will return the same object during a single page load
+ *
+ * @return object Services_Twilio
+ */
+function get_twilio_service() {
+	static $twilio_service;
+
+	if (!($twilio_service instanceof Services_Twilio)) {
+		/**
+		 * This assumes that you've defined your SID & TOKEN as constants
+		 * Replace with a way to get your SID & TOKEN if different
+		 */
+                $twsid = ""; // Your Twilio account sid
+                $twtoken = ""; // Your Twilio auth token
+		$twilio_service = new Services_Twilio($twsid, $twtoken);
+	}
+
+	return $twilio_service;
+}
+
+?>
