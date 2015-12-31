cba932364ffde0b97a95150fbb186b8717edd4a4
Indra Kurniawan <indramgl@users.noreply.github.com>
2015-12-31 10:46:16 +0700

    Task #26 - Integrate Reserv with Mandrill API
    Added Mandrill API Library

--- application/controllers/report.php
+++ application/controllers/report.php
@@ -856,8 +856,99 @@ class Report extends CI_Controller {
         
         public function mail()
         {
+            $this->load->helper('mandrill');
+            $service    = get_mandrill_service();
             
-            $this->output->set_output($output);
+            $message = array(
+                'html' => '<p>Example HTML content</p>',
+                'text' => 'Example text content',
+                'subject' => 'example subject',
+                'from_email' => 'no-reply@solusi-integral.co.id',
+                'from_name' => 'Solusi Integral',
+                'to' => array(
+                    array(
+                        'email' => 'indra@indramgl.web.id',
+                        'name' => 'Indra Kurniawan',
+                        'type' => 'to'
+                    )
+                ),
+                'headers' => array('Reply-To' => 'support@solusi-integral.co.id'),
+                'important' => false,
+                'track_opens' => true,
+                'track_clicks' => true,
+                'auto_text' => null,
+                'auto_html' => null,
+                'inline_css' => null,
+                'url_strip_qs' => null,
+                'preserve_recipients' => null,
+                'view_content_link' => null,
+                //'bcc_address' => 'message.bcc_address@example.com',
+                'tracking_domain' => null,
+                'signing_domain' => null,
+                'return_path_domain' => null,
+                'merge' => true,
+                'merge_language' => 'mailchimp',
+                /**'global_merge_vars' => array(
+                    array(
+                        'name' => 'merge1',
+                        'content' => 'merge1 content'
+                    )
+                ),
+                'merge_vars' => array(
+                    array(
+                        'rcpt' => 'recipient.email@example.com',
+                        'vars' => array(
+                            array(
+                                'name' => 'merge2',
+                                'content' => 'merge2 content'
+                            )
+                        )
+                    )
+                ),*/
+                'tags' => array('reserv'),
+                'subaccount' => 'reserv',
+                //'google_analytics_domains' => array('example.com'),
+                //'google_analytics_campaign' => 'message.from_email@example.com',
+                //'metadata' => array('website' => 'www.example.com'),
+                /**'recipient_metadata' => array(
+                    array(
+                        'rcpt' => 'recipient.email@example.com',
+                        'values' => array('user_id' => 123456)
+                    )
+                ),
+                'attachments' => array(
+                    array(
+                        'type' => 'text/plain',
+                        'name' => 'myfile.txt',
+                        'content' => 'ZXhhbXBsZSBmaWxl'
+                    )
+                ),
+                'images' => array(
+                    array(
+                        'type' => 'image/png',
+                        'name' => 'IMAGECID',
+                        'content' => 'ZXhhbXBsZSBmaWxl'
+                    )
+                )*/
+            );
+            $async = false;
+            $ip_pool = 'Main Pool';
+            //$send_at = 'example send_at';
+            $result = $service->messages->send($message, $async, $ip_pool, $send_at);
+            print_r($result);
+            /*
+            Array
+            (
+                [0] => Array
+                    (
+                        [email] => recipient.email@example.com
+                        [status] => sent
+                        [reject_reason] => hard-bounce
+                        [_id] => abc123abc123abc123abc123abc123
+                    )
+
+            )
+            */
         }
 }
 
