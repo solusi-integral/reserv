a215fd2f6bbef5593c9cdd9404abb255dbedfb43
Indra Kurniawan <indramgl@users.noreply.github.com>
2015-12-31 10:25:59 +0700

    Added Mandrill API Library

---
+++ application/helpers/mandrill_helper.php
@@ -0,0 +1,31 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+if (!class_exists('Services_Twilio')) {
+	/**
+	 * The main Twilio.php file contains an autoload method for its dependent
+	 * classes, we only need to include the one file manually.
+	 */
+	include_once(APPPATH.'libraries/Services/Mandrill.php');
+}
+
+/**
+ * Return a twilio services object.
+ *
+ * Since we don't want to create multiple connection objects we
+ * will return the same object during a single page load
+ *
+ * @return object Services_Twilio
+ */
+function get_mandrill_service() {
+	static $mandrill_service;
+
+	$mandrill_service = new Mandrill('YOUR_API_KEY');
+
+	return $mandrill_service;
+}
